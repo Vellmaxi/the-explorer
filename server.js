5c10171367c5fafd5712305a64d73ec2cb196b6e
faidhon.nur <faidhon.nur@samsung.com>
2025-11-24 12:23:40 +0700

    fix: project baseURL to enable using remote server

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const path = require('path');
 
 const app = express();
 app.use(express.json());
+
+// CORS middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 const PORT = 3000;
 let PROJECT_ROOT_API = path.normalize('C:/');
 const PROJECT_ROOT_UI = __dirname;
